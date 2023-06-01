9706e14d1c65f03f1d2d96881c96f1db3c745986
Simmi Badhan <simmi@addvalsolutions.com>
2023-06-01 16:43:20 +0530

    Flash messages chapter 7

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,6 +1,8 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  include Warden::Test::Helpers
+
   # Change :chrome with :headless_chrome
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 end
